677a667140ffaa32e8c3a10059152baa91277eea
moyoussef11 <mohmedyoussef472@gmail.com>
2025-08-17 20:46:50 +0300

    change path dir

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 import hero from "../assets/hero.jpg";
 import Categories from "../components/Posts/Categories";
 import useHome from "../hooks/useHome";
-import Post from "../components/Posts/post";
-
+import Post from "../components/Posts/Post";
 
 const Home = () => {
   const { posts } = useHome();
